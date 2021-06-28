ebd8988df6be478eb156bf089c471f108fc05016
Marshall Miller <contact@marshall-miller.com>
2021-06-28 15:59:41 -0400

    Update InvoiceShipperDetails.php

--- src/InvoiceShipperDetails.php
+++ src/InvoiceShipperDetails.php
@@ -7,4 +7,15 @@
 	*
 	* @author Marshall Miller
 	*/
-	class InvoiceShipperDetails extends \BrokenTitan\DPD\InvoiceDetails {}
+	class InvoiceShipperDetails extends \BrokenTitan\DPD\InvoiceDetails {
+		/**
+		 * @method toArray
+		 * @return array
+		 */
+		public function toArray() : array {
+			$values = parent::toArray();
+			unset($values["pidNumber"]); //The pidNumber will never be set for the shipper.
+			
+			return $values;
+		}
+	}
